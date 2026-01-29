c7455cee702f0824f03cdd6e51a1d5af2834711f
Rainfall1831 <fahibsyed@gmail.com>
2026-01-28 23:30:33 -0500

    Update carousel, add images, enable auto-scroll, and set site background to black

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 'use client'
 
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
+import AutoScroll from 'embla-carousel-auto-scroll'
 
-export default function EmblaCarousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+export default function Page() {
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true },
+    [AutoScroll({ speed: 1.3, startDelay: 0 })]
+  )
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.plugins().autoScroll?.play()
+    }
+  }, [emblaApi])
 
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev()
@@ -15,19 +24,24 @@ export default function EmblaCarousel() {
     if (emblaApi) emblaApi.scrollNext()
   }, [emblaApi])
 
+  // List of images in public/images
+  const images = [
+    '/images/babyshower12.jpeg',
+    '/images/babyshower13.jpeg',
+    '/images/babyshower14.jpeg',
+    '/images/Ribbon-ex2.png',
+    '/images/wedding1.webp',
+  ]
+
   return (
-    <div className='embla mx-auto mt-12 max-w-lg'>
+    <div className='embla mx-auto mt-12 w-full max-w-7xl'>
       <div className='embla__viewport h-56 border' ref={emblaRef}>
         <div className='embla__container h-full'>
-          <div className='embla__slide flex items-center justify-center'>
-            Slide 1
-          </div>
-          <div className='embla__slide flex items-center justify-center'>
-            Slide 2
-          </div>
-          <div className='embla__slide flex items-center justify-center'>
-            Slide 3
-          </div>
+          {images.map((src, idx) => (
+            <div key={src} className='embla__slide flex items-center justify-center'>
+              <img src={src} alt={`Slide ${idx + 1}`} className='max-h-full max-w-full object-contain' />
+            </div>
+          ))}
         </div>
       </div>
 
